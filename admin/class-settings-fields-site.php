5ba1a1950caec3ab3e01483f15cdd86955bc47a2
Greg Sweet <greg@ccdzine.com>
2018-07-10 20:47:29 -0800

    Added a field to parent settings page
    A checkbox was added to the Site Settings page of the parent plugin. It currenty does nothing but will hide the addon settings page.

---
+++ admin/class-settings-fields-site.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Settings for site customization.
+ *
+ * Extends the parent plugin site settings.
+ *
+ * @package    Controlled_Chaos_Addon
+ * @subpackage Admin
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+
+namespace CC_Addon\Admin;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Settings for site customization.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Settings_Fields_Site {
+
+	/**
+	 * Holds the values to be used in the fields callbacks.
+	 *
+	 * @var array
+	 */
+	private $options;
+
+	/**
+	 * Get an instance of the plugin class.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return object Returns the instance.
+	 */
+	public static function instance() {
+
+		// Varialbe for the instance to be used outside the class.
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+
+			// Set variable for new instance.
+			$instance = new self;
+
+		}
+
+		// Return the instance.
+		return $instance;
+
+	}
+
+	/**
+	 * Constructor method.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+    public function __construct() {
+
+		// Register settings sections and fields.
+		if ( is_admin() ) {
+			add_action( 'admin_init', [ $this, 'settings' ], 11 );
+		}
+
+	}
+
+	/**
+	 * Plugin site settings.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 *
+	 * @link  https://codex.wordpress.org/Settings_API
+	 */
+	public function settings() {
+
+		/**
+		 * Admin menu settings.
+		 *
+		 * @since 1.0.0
+		 */
+
+		// Site Settings page position.
+		add_settings_field(
+			'cca_hide_addon_settings',
+			__( 'Hide Addon Settings', 'controlled-chaos-addon' ),
+			[ $this, 'cca_hide_addon_settings_callback' ],
+			'ccp-site-admin-menu',
+			'ccp-site-admin-menu',
+			[ sprintf(
+				'%1s %2s %3s',
+				esc_html__( 'Hide the', 'controlled-chaos-addon' ),
+				CCA_CHILD_NAME,
+				esc_html__( 'link in the admin menu', 'controlled-chaos-addon' )
+			) ]
+		);
+
+		register_setting(
+			'ccp-site-admin-menu',
+			'cca_hide_addon_settings'
+		);
+
+	}
+
+	/**
+	 * Site Settings page position.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  array $args Extra arguments passed into the callback function.
+	 * @return string
+	 */
+	public function cca_hide_addon_settings_callback( $args ) {
+
+		$option = get_option( 'cca_hide_addon_settings' );
+
+		$html = '<p><input type="checkbox" id="cca_hide_addon_settings" name="cca_hide_addon_settings" value="1" ' . checked( 1, $option, false ) . '/>';
+
+		$html .= '<label for="cca_hide_addon_settings"> '  . $args[0] . '</label></p>';
+
+		echo $html;
+
+	}
+
+}
+
+/**
+ * Put an instance of the class into a function.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return object Returns an instance of the class.
+ */
+function cca_settings_fields_site() {
+
+	return Settings_Fields_Site::instance();
+
+}
+
+// Run an instance of the class.
+cca_settings_fields_site();
--- admin/class-settings.php
+++ admin/class-settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Admin functiontionality and settings.
+ * The core settings class for the plugin.
  *
  * @package    Controlled_Chaos_Addon
  * @subpackage Admin
@@ -16,18 +16,13 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Bail if not in the admin.
-if ( ! is_admin() ) {
-	return;
-}
-
 /**
  * Admin functiontionality and settings.
  *
  * @since  1.0.0
  * @access public
  */
-class Admin {
+class Settings {
 
 	/**
 	 * Get an instance of the plugin class.
@@ -61,14 +56,9 @@ class Admin {
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 * @return self
+	 * @return void Constructor method is empty.
 	 */
-	public function __construct() {
-
-		// Add an about page for the plugin.
-        add_action( 'admin_menu', [ $this, 'settings_page' ] );
-
-	}
+	public function __construct() {}
 
 	/**
 	 * Class dependency files.
@@ -79,45 +69,16 @@ class Admin {
 	 */
 	private function dependencies() {
 
-		// The core settings class for the plugin.
-		require_once plugin_dir_path( __FILE__ ) . 'class-settings.php';
+		/**
+		 * Settings fields.
+		 *
+		 * @since  1.0.0
+		 */
+
+		// Settings fields for site customization.
+		require_once plugin_dir_path( __FILE__ ) . 'class-settings-fields-site.php';
 
 	}
-
-	/**
-     * Add a settings page for the plugin.
-     *
-     * Uses the universal slug partial for admin pages. Set this
-     * slug in the core plugin file.
-     *
-     * @since  1.0.0
-	 * @access public
-	 * @return void
-     */
-    public function settings_page() {
-
-		add_options_page(
-			CCA_CHILD_NAME,
-			CCA_CHILD_NAME,
-			'manage_options',
-			CCA_ADMIN_SLUG . '-settings',
-			[ $this, 'settings_page_output' ]
-		);
-
-	}
-
-	/**
-     * Get output of the settings page for the plugin.
-     *
-     * @since  1.0.0
-	 * @access public
-	 * @return void
-     */
-    public function settings_page_output() {
-
-        require plugin_dir_path( __FILE__ ) . 'partials/plugin-settings_page.php';
-
-    }
 
 }
 
@@ -128,11 +89,11 @@ class Admin {
  * @access public
  * @return object Returns an instance of the class.
  */
-function cca_admin() {
+function cca_settings() {
 
-	return Admin::instance();
+	return Settings::instance();
 
 }
 
 // Run an instance of the class.
-cca_admin();
+cca_settings();
